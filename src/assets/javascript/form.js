fd513e784134dd6933e1acd9602c94c32f2193e8
Luma Szfranski <lumaszfranski@gmail.com>
2022-08-19 22:49:20 -0300

    modified:   src/app/app.component.html 	modified:   src/assets/javascript/form.js

--- src/assets/javascript/form.js
+++ src/assets/javascript/form.js
@@ -2,29 +2,10 @@ var botaoAdicionar = document.querySelector("#botao-adiciona")
 console.log(botaoAdicionar);
 botaoAdicionar.addEventListener("click", function(event) {
     event.preventDefault();
-    
+ 
+    salvarConsulta();
 
-    var form = document.querySelector("#form-adiciona");
-    var cliente = obtemClienteDoFormulario(form);
-    var erros = validaCliente(cliente);
-
-    if (erros.length > 0 ) {
-        exibeMensagensDeErro(erros);
-        return;
-    }
-
-    adicionaClienteNaTabela(cliente);
-
-  
-    var mensagensErro = document.querySelector("#mensagens-erro");
-    mensagensErro.innerHTML = "";
 });
-
-
-
-localStorage.setItem('nome', 'jean');
-
-
 
 if(localStorage.nome) {
     document.getElementById('nome').value = localStorage.nome;
@@ -34,13 +15,13 @@ if(localStorage.endereco) {
     document.getElementById('rua').value = localStorage.rua;
 }
 
-if(localStorage.nome) {
+if(localStorage.email) {
     document.getElementById('email').value = localStorage.email;
 }
 
 var salvarConsulta = function() {
     var nome = document.getElementById('nome').value;
-    var endereco = document.getElementById('rua').value;
+    var rua = document.getElementById('rua').value;
     var email = document.getElementById('email').value;
 
     localStorage.setItem('nome', nome);
